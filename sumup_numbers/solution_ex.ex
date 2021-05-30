ed8e4217dc233633e3b548b3ba72a7544812ccf0
sahinyasasi <sahinyasasi@example.com>
2021-05-30 11:32:36 +0530

    sumup_numbers

---
+++ sumup_numbers/solution_ex.ex
@@ -0,0 +1,5 @@
+defmodule Sum do
+  def sum(num) do
+    Enum.sum(1..num)
+  end
+end
---
+++ sumup_numbers/solution_js.js
@@ -0,0 +1,7 @@
+function Sum(num) {
+  let suml = 0;
+  for (let i = 1; i <= num; i++) {
+    suml += i;
+  }
+  return suml;
+}
