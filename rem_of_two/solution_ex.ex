a3b7448eaa345fa1f28b36677e865debe575a523
sahinyasasi <sahinyasasi@example.com>
2021-05-30 09:53:24 +0530

    remiander_of_two_numbers

---
+++ rem_of_two/solution_ex.ex
@@ -0,0 +1,5 @@
+defmodule Rem do
+  def remainder(x, y) do
+    rem(x, y)
+  end
+end
---
+++ rem_of_two/solution_js.js
@@ -0,0 +1,7 @@
+function remainder(x, y) {
+  if (Number.isInteger(x) && Number.isInteger(y)) {
+    return x % y;
+  } else {
+    console.log("enter integer");
+  }
+}
